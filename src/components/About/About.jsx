d5f98c03e818690bac2dd25f3504042cf7b8bb80
vaishnavichaudhari910 <vaishnavichaudhari910@gmail.com>
2026-05-03 17:44:25 +0530

    Inital Commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import Tilt from 'react-parallax-tilt';
+import profileImage from '../../assets/vaishnavi_circle.png';
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[4vw] font-sans mt-16 md:mt-24 lg:mt-10"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            Vaishnavi Chaudhari
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a </span>
+            <TypeAnimation
+  sequence={[
+     'MERN Stack Developer', 2000,
+    'Full Stack Developer', 2000,
+    'React Developer', 2000,
+    'Node.js Developer', 2000,
+  ]}
+  wrapper="span"
+  speed={50}
+  repeat={Infinity}
+/>
+          </h3>
+          {/* About Me Paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            I am a passionate Full Stack Developer and MCA student.
+             I specialize in the MERN stack and enjoy building modern, responsive, and user-friendly web applications. 
+             I have worked on projects like AI SaaS platforms, portfolio websites, and real-world full stack applications. 
+             With 3 months of internship experience, I have gained practical exposure to development workflows, teamwork, and real-time project execution.
+          </p>
+          {/* Resume Button */}
+          <a
+            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+          
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <Tilt
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Vaishnavi Chaudhari"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,207 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import Clerk from './assets/tech_logo/Clerk.png';
+
+// Experience Section Logo's
+import uslogo from './assets/company_logo/uslogo.webp';
+
+// Education Section Logo's
+import IMRLogo from './assets/education_logo/IMRLogo.png';
+import DNVKLogo from './assets/education_logo/DNVKLogo.jpeg';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import Prompta from './assets/work_logo/Prompta.png';
+import QueryBot from './assets/work_logo/QueryBot.png';
+import snake from './assets/work_logo/snake.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Clerk', logo: Clerk},
+      
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: uslogo,
+      role: "MERN Stack Internship",
+      company: "UpToSkills",
+      date: "January 2026 - April 2026",
+      desc: "Worked as a Full Stack Developer Intern using the MERN stack, contributing to the development of responsive and user-friendly web applications. Assisted in building frontend components with React and developing backend APIs using Node.js and Express.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+
+      ],
+    },
+   
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: IMRLogo,
+      school: "KCES Institute of Management And Research,Jalgaon",
+      date: "June 2021 - June 2026",
+      grade: "9.63 CGPA",
+      desc: "Pursuing an Integrated Master of Computer Applications (IMCA) from KCES Institute of Management and Research, Jalgaon, with a strong academic record of 9.63 CGPA. Gained a solid foundation in core computer science subjects including Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, and Web Development. Developed practical skills in full-stack development (MERN) through hands-on projects and continuous learning. Actively engaged in building real-world applications, enhancing problem-solving abilities, and preparing for a career in software development.",
+      degree: "Integrated Master of Computer Applications - IMCA",
+    },
+    {
+      id: 1,
+      img: DNVKLogo,
+      school: "Dhanaji Nana Vidyalaya,Khiroda",
+      date: "February 2021",
+      grade: "87.67%",
+      desc:"Completed Higher Secondary Education (12th) from Dhanaji Nana Vidyalaya, Khiroda with 87.67%. Strengthened core knowledge in subjects like Mathematics and Science, building a solid academic base for pursuing computer applications. Developed analytical thinking, problem-solving skills, and a keen interest in technology and programming.",
+        degree: "12th",
+    },
+    {
+      id: 2,
+      img: DNVKLogo,
+      school: "Dhanaji Nana Vidyalaya,Khiroda",
+      date: "March 2019",
+      grade: "86.20%",
+      desc: "Completed Secondary School (10th) from Dhanaji Nana Vidyalaya, Khiroda with 86.20%. Built a strong academic foundation in core subjects including Mathematics, Science, and English. Developed analytical thinking, discipline, and problem-solving skills, which laid the groundwork for further studies in computer science and technology.",
+      degree: "10th",
+    },
+   
+  ];
+  
+  export const projects = [
+    {
+      
+  id: 0,
+  title: "Prompta AI | Full-Stack AI Content and Media SaaS Platform",
+  description:
+    "Developed an AI-powered SaaS platform offering 5+ features including article writing, image generation, background removal, resume analysis, and object removal. Built a responsive and intuitive frontend using React, Tailwind CSS, and React Router with 10+ user-facing screens. Implemented secure authentication and subscription-based access (free and premium) using Clerk. Integrated Gemini APIs for AI content generation and managed image uploads and processing using Cloudinary and Multer.",
+  image: Prompta,
+  tags: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React JS",
+    "Tailwind CSS",
+    "Node JS",
+    "Express JS",
+    "PostgreSQL",
+    "Clerk",
+    "Gemini API",
+    "Cloudinary",
+  ],
+  github: "https://github.com/vaishnavichaudhari910/Prompta_AI",
+  webapp: "https://prompta-ai.vercel.app/",
+},
+   {
+  id: 1,
+  title: "QueryBot AI | Chat and Image Generation Platform",
+  description:
+    "Engineered an AI-driven chat and image generation platform using the MERN stack, enabling real-time conversations and AI-assisted image creation. Integrated Gemini APIs to generate intelligent responses while managing chat history and media outputs efficiently for multiple users. Implemented JWT-based authentication and secured APIs, along with a credit-based premium subscription model using Stripe payments and webhook handling for monetization.",
+  image: QueryBot,
+  tags: [
+    "React JS",
+    "Node.js",
+    "Express",
+    "MongoDB",
+    "JavaScript",
+    "JWT",
+    "Gemini API",
+    "Stripe",
+  ],
+  github: "https://github.com/vaishnavichaudhari910/QueryBot",
+  webapp: "https://query-bot-tawny.vercel.app/",
+},
+    {
+      id: 2,
+      title: "Snake Game",
+      description:
+        "A classic Snake Game built using JavaScript, where players control the snake to eat food, grow longer, and avoid collisions.",
+      image: snake,
+      tags: [ "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/vaishnavichaudhari910/Snake_Game",
+      webapp: "https://snake-game-real.netlify.app/",
+    },
+    
+  ];  
